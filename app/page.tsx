29ec7fffaffa50dde16d10eb8fb89e7492abb637
Amit Singh <developer@digisutrasolutions.com>
2026-03-27 17:48:38 +0530

    fixes

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
   return (
     <div>
       <Hero />
-      <HeroStatement />
+      {/* <HeroStatement /> */}
       <HomeListings />
       <AboutUs />
       <SignatureProjects />
